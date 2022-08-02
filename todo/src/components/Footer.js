b24ecb11c10718d1970dabc88fe335a51deb818b
Elif <elffemre@gmail.com>
2022-08-02 16:18:07 +0300

    genel görümün hazırlanması

---
+++ todo/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+   <footer className='info'>
+    <p>Click to edit a todo</p>
+    <p>
+        Part of <a href='http://todomvc.com'>TodoMVC</a>
+    </p>
+   
+   </footer>
+  )
+}
+
+export default Footer
---
+++ todo/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <form>
+        <input className='new-todo' placeholder="what need to be done?" autoFocus></input>
+    </form>
+  )
+}
+
+export default Form
